78f5ba57208bf1a2db2c62235adfa821b39fb385
Bryson Kruk <brysonkruk@Brysons-MacBook-Air.local>
2018-09-03 11:12:57 -0600

    added block explorer, full wallet app, and reorganized marketing site

---
+++ wallet/app/containers/Header/network.js
@@ -0,0 +1,10 @@
+
+const Network = {
+  Offline: { rpc: 'offline', tx_explorer: null },
+  'Remote Custom RPC': { rpc: 'http://35.227.52.90:8111', tx_explorer: null },
+  'Local RPC': { rpc: 'http://127.0.0.1:8545', tx_explorer: null },
+  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
+  'Main Net': { rpc: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://etherscan.io/tx/' },
+};
+
+module.exports = Network;

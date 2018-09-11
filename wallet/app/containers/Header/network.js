747af866113678b4acf8f6dd97694ea3285a96bb
Bryson Kruk <brysonkruk@Brysons-MacBook-Air.local>
2018-09-11 17:42:42 -0600

    added basic dashboard

--- wallet/app/containers/Header/network.js
+++ wallet/app/containers/Header/network.js
@@ -1,8 +1,8 @@
 
 const Network = {
   Offline: { rpc: 'offline', tx_explorer: null },
-  'Remote Custom RPC': { rpc: 'http://35.227.52.90:8111', tx_explorer: null },
-  'Local RPC': { rpc: 'http://127.0.0.1:8545', tx_explorer: null },
+  'Remote Custom RPC': { rpc: 'https://35.227.52.90:8111', tx_explorer: null },
+  'Local RPC': { rpc: 'http://127.0.0.1:7545', tx_explorer: null },
   'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
   'Main Net': { rpc: 'https://mainnet.infura.io/GjiCzFxpQAUkPtDUpKEP', tx_explorer: 'https://etherscan.io/tx/' },
 };

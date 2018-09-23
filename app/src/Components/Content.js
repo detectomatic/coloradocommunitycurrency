c31f8f49eb3fb7842c17f6a728144517036e088c
Bryson Kruk <brysonkruk@c-75-71-255-19.hsd1.co.comcast.net>
2018-09-23 11:29:01 -0600

    added login and register pages

--- app/src/Components/Content.js
+++ app/src/Components/Content.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import { NotificationContainer } from 'react-notifications';
 import TransactionTable from '~/Components/TransactionTable/TransactionTable';
+import Account from '~/Components/Account/Account';
+import Login from '~/Components/Login/Login';
+import Register from '~/Components/Register/Register';
 import Demo from '~/Components/Demo';
 
 
@@ -16,6 +19,9 @@ export default class Content extends React.Component{
               <Route path={`/contact`} />
               <Route path={`/explorer`} />
               <Route path={`${APP_ROOT}transactions`} component={() => ( <TransactionTable createNotification={this.props.createNotification} dummyTransactions={this.props.dummyTransactions} /> )} />
+              <Route path={`${APP_ROOT}account`} component={() => ( <Account createNotification={this.props.createNotification} dummyTransactions={this.props.dummyTransactions} /> )} />
+              <Route path={`${APP_ROOT}login`} component={() => ( <Login createNotification={this.props.createNotification} dummyTransactions={this.props.dummyTransactions} /> )} />
+              <Route path={`${APP_ROOT}register`} component={() => ( <Register createNotification={this.props.createNotification} dummyTransactions={this.props.dummyTransactions} /> )} />
               <Route path={`${APP_ROOT}demo`} component={() => ( <Demo sendEther={this.props.sendEther} readBalance={this.props.readBalance} />)} />
           </Switch>
         </div>

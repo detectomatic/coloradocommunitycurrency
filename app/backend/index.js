bfb9960371a2d43a3eed9d83b730e828bd9b487f
Bryson Kruk <brysonkruk@c-75-71-255-19.hsd1.co.comcast.net>
2018-10-17 10:47:28 -0600

    cors tweak

--- app/backend/index.js
+++ app/backend/index.js
@@ -20,7 +20,8 @@ const sessionStore = require('./store').sessionStore;
 app.listen( process.env.PORT || 3001, function () {
     console.log('Listening on port ' + (process.env.PORT || 3001));
     // Middleware
-    app.use(cors({credentials: true, origin: true}));
+    //app.use(cors({credentials: true, origin: true}));
+    app.use(cors({credentials: true, origin: 'https://beta-dot-dcoin-web-app.appspot.com/'}));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(express.static(path.join(__dirname, 'public')));
@@ -29,11 +30,11 @@ app.listen( process.env.PORT || 3001, function () {
     app.use(sessionStore);
     app.use(passport.initialize());
     app.use(passport.session());
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-    });
+    // app.use(function(req, res, next) {
+    //   res.header("Access-Control-Allow-Origin", "*");
+    //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //   next();
+    // });
     require('./auth.js')(passport, LocalStrategy);
     
     // HTTP Routes

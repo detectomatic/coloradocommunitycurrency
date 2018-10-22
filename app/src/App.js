a545f5888ec3f2275436775ccb9891991f71c994
Bryson Kruk <brysonkruk@c-75-71-255-19.hsd1.co.comcast.net>
2018-10-22 10:38:40 -0600

    a

--- app/src/App.js
+++ app/src/App.js
@@ -216,8 +216,8 @@ class App extends React.Component{
   // If logged in, set state to the user's data
   componentWillMount(){ 
     //const cookieName = APP_MODE === 'development' ? 'sid' : '1P_JAR';
-    const cookie = this.props.cookies.get('sid');
-    if(cookie !== undefined) {
+    // const cookie = this.props.cookies.get('sid');
+    // if(cookie !== undefined) {
       loggedIn()
       .then((data) =>{
         console.log('DATA', data);
@@ -226,7 +226,7 @@ class App extends React.Component{
           this._initWeb3();
         }
       })
-    } 
+    //} 
   }
 
   // Render APP Component

e1bde3482d638a1bf2f791d8f9e4fb7c1629e2c4
Bryson Kruk <brysonmk1984@gmail.com>
2018-11-11 11:32:30 -0700

    asd

--- app/src/common/web3Service.js
+++ app/src/common/web3Service.js
@@ -21,7 +21,7 @@ const retrieveTransactionData = function(transArray){
 // Read Balance of account off blockchain after hitting node backend
 const readBalance = function(publicEthKey){
   return new Promise((resolve, reject) =>{
-      return axios.post(`${endpoint}read-balance`, { publicEthKey }, { withCredentials:true })
+      return axios.post(`${endpoint}read-balance`, { publicEthKey : encodeURIComponent(publicEthKey) }, { withCredentials:true })
       .then((data) => {
           console.log('Balance from account', data);
           resolve(data.data);

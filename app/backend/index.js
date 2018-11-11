73eddf73b3c11b2c538538401214d100da6b7724
Bryson Kruk <brysonmk1984@gmail.com>
2018-11-11 10:42:12 -0700

    asd

--- app/backend/index.js
+++ app/backend/index.js
@@ -36,6 +36,7 @@ app.listen( process.env.PORT || 3001, function () {
   app.use(passport.session());
   require('./auth.js')(passport, LocalStrategy);
   
+  app.options('*', cors({credentials: true, origin: process.env.NODE_ENV === 'production' ? 'https://dcoin-web-app.appspot.com' : 'http://localhost:8080'}))
   // HTTP Routes
   app.use('/transactions', transactionRoutes);
   app.use('/users', userRoutes);
